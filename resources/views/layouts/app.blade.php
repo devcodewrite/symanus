a414211c338ce55a2bf8fa1f2bb1e1e42b8b5647
Eric Mensah <ericmensah@codewrite.org>
2023-02-18 00:39:45 +0000

    updated js and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     @yield('style', '')
 
     <!-- Base Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js?v=1') }}" defer></script>
     
     <!-- Custom Scripts -->
     @yield('script', '')
